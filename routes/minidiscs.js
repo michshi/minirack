516fdd1ea691e512f551c87397534c153b67572c
shieldsy123 <michael.t.shields@gmail.com>
2018-04-03 14:30:45 -0600

    working API with knex

--- routes/minidiscs.js
+++ routes/minidiscs.js
@@ -4,27 +4,96 @@ const knex = require('../knex')
 
 // READ ALL records for this table
 router.get('/', (req, res, next) => {
-  res.send('ALL RECORDS')
+  // res.send('ALL RECORDS')
+  knex('minidiscs')
+    .then((rows) => {
+      res.json(rows)
+    })
+    .catch((err) => {
+      next(err)
+    })
 })
 
 // READ ONE record for this table
 router.get('/:id', (req, res, next) => {
-  res.send('ONE RECORD')
+  // res.send('ONE RECORD')
+  knex('minidiscs')
+    .where('id', req.params.id)
+    .then((rows) => {
+      res.json(rows)
+    })
+    .catch((err) => {
+      next(err)
+    })
 })
 
 // CREATE ONE record for this table
 router.post('/', (req, res, next) => {
-  res.send('CREATED RECORD')
+  // res.send('CREATED RECORD')
+  knex('minidiscs')
+    .insert({
+      "title": req.body.title,
+      "artist": req.body.artist,
+      "genre": req.body.genre,
+      "description": req.body.description,
+      "cover_url": req.body.cover_url
+    })
+    .returning('*')
+    .then((data) => {
+      res.json(data[0])
+    })
+    .catch((err) => {
+      next(err)
+    })
 })
 
 // UPDATE ONE record for this table
 router.put('/:id', (req, res, next) => {
-  res.send('UPDATED RECORD')
+  // res.send('UPDATED RECORD')
+  knex('minidiscs')
+    .where('id', req.params.id)
+    .then((data) => {
+      if (!data) return next()
+
+      knex('minidiscs')
+      .where('id', req.params.id)
+      .limit(1)
+      .update({
+        "title": req.body.title,
+        "artist": req.body.artist,
+        "genre": req.body.genre,
+        "description": req.body.description,
+        "cover_url": req.body.cover_url
+      })
+      .returning('*')
+      .then((data) => {
+        res.json(data[0])
+      })
+    })
+    .catch((err) => {
+      next(err)
+    })
 })
 
 // DELETE ONE record for this table
 router.delete('/:id', (req, res, next) => {
-  res.send('DELETED RECORD')
+  // res.send('DELETED RECORD')
+  knex('minidiscs')
+    .where('id', req.params.id)
+    .first()
+    .then((row) => {
+      if(!row) return next()
+
+      knex('minidiscs')
+        .del()
+        .where('id', req.params.id)
+        .then(() => {
+          res.send(`ID ${req.params.id} Deleted`)
+        })
+    })
+    .catch((err) => {
+      next(err)
+    })
 })
 
 module.exports = router
